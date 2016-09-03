02d0d0655743b8727efd16c7bc848271386404f5
André Scholz (sneakyx) <info@rothaarsystems.de>
2016-09-03 17:47:11 +0200

    hopefully errors deleted

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,26 @@
+
 FROM php:5.6-apache
 MAINTAINER André Scholz <info@rothaarsystems.de>
-# Version 2016-09-03
 
 ENV DEBIAN_FRONTEND noninteractive
-ARG HOST_NAME=""
 ARG egr_timezone=Europe/Berlin
+#ARG HOST_NAME=""
 RUN apt-get update \
         && apt-get install -y wget bzip2 zlib1g-dev re2c libmcrypt-dev pwgen dnsutils
-RUN a2enmod proxy_http proxy_ajp proxy_balancer rewrite headers ssl       
+RUN a2enmod proxy_http proxy_ajp proxy_balancer rewrite headers ssl
+RUN touch /usr/local/etc/php/conf.d/uploads.ini \
+    && echo date.timezone = $egr_timezone  >> /usr/local/etc/php/conf.d/uploads.ini \
+    && echo session.save_path = /var/tmp  >> /usr/local/etc/php/conf.d/uploads.ini
 
-RUN mkdir --parents /var/keys/
-RUN mkdir --parents /var/log/apache2/
 
-COPY docker-entrypoint.sh /entrypoint.sh 
+COPY docker-entrypoint.sh /entrypoint.sh
 COPY apache.conf /etc/apache2/apache2.conf
+# COPY assets/apache-with*.conf /etc/apache2/sites-available/
 
+# there are two updated files
+# because manual installation of egroupware leaves some infos blank
 
-RUN chmod +x /entrypoint.sh  
+RUN chmod +x /entrypoint.sh
 
 EXPOSE 80 443
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,30 +2,37 @@
 set -e
 # made by sneaky of Rothaar Systems (Andre Scholz)
 # V2016-09-03-15-45
-touch /etc/apache2/sites-available/000-default.conf
+echo "<VirtualHost *:80>" > /etc/apache2/sites-available/000-default.conf
+echo "ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/000-default.conf
+echo "DocumentRoot /var/www/html" >> /etc/apache2/sites-available/000-default.conf
+echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
 
-if [ -z $1 ]; then
+if [ -n $HOST_NAME ]; then
 
-	# Apache gets grumpy about PID files pre-existing
-	rm -f /var/run/apache2/apache2.pid
-	service apache2 start
-	mkdir --parents /var/keys/$1
-	cd /root
-	curl https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh >acme.sh
-	chmod +x ./acme.sh
-	./acme.sh --issue -d $1 -w /var/www/html/
-	a2dissite 000-default.conf
-	a2ensite apache-without-ssl 
-	./acme.sh --installcert -d $1 \
-	--certpath /var/keys/$1/site.cer \
-	--keypath  /var/keys/$1/site.key  \
-	--capath   /var/keys/$1/ca.cer  \
-	--fullchainpath /var/keys/$1/fullchain.cer 
-	#--reloadcmd  "service apache2 reload"
-	a2ensite apache-with-ssl
-	service apache2 stop
-fi # if there should be something (new) registered	
+        # Apache gets grumpy about PID files pre-existing
+        rm -f /var/run/apache2/apache2.pid
+        service apache2 start
+        mkdir --parents /var/keys/$HOST_NAME
+        cd /root
+        curl https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh >acme.sh
+        chmod +x ./acme.sh
+        ./acme.sh --issue -d $HOST_NAME -w /var/www/html/
+        a2dissite 000-default.conf
+        a2ensite apache-without-ssl 
+        ./acme.sh --installcert -d $HOST_NAME \
+        --certpath /var/keys/$HOST_NAME/site.cer \
+        --keypath  /var/keys/$HOST_NAME/site.key  \
+        --capath   /var/keys/$HOST_NAME/ca.cer  \
+        --fullchainpath /var/keys/$HOST_NAME/fullchain.cer 
+        #--reloadcmd  "service apache2 reload"
+        a2ensite apache-with-ssl
+        service apache2 stop
 
+else
+        a2dissite 000-default.conf
+        a2ensite apache-without-ssl 
+        a2ensite apache-with-ssl
+fi # if there should be something (new) registered
 rm -f /var/run/apache2/apache2.pid
 exec apache2 -DFOREGROUND
 exit 0
