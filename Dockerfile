fd37876408b62b1a294ecc51eb271a36d6eb0c91
André Scholz (sneakyx) <info@rothaarsystems.de>
2016-09-24 00:11:25 +0200

    new veriosn- just still testing! not for productive!

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,28 @@
-
 FROM php:5.6-apache
 MAINTAINER André Scholz <info@rothaarsystems.de>
+# Version 2016-09-24
 
 ENV DEBIAN_FRONTEND noninteractive
-ARG egr_timezone=Europe/Berlin
-#ARG HOST_NAME=""
+ARG HOST_NAME=""
+ARG ACTION=""
 RUN apt-get update \
         && apt-get install -y wget bzip2 zlib1g-dev re2c libmcrypt-dev pwgen dnsutils
-RUN a2enmod proxy_http proxy_ajp proxy_balancer rewrite headers ssl
 RUN touch /usr/local/etc/php/conf.d/uploads.ini \
     && echo date.timezone = $egr_timezone  >> /usr/local/etc/php/conf.d/uploads.ini \
     && echo session.save_path = /var/tmp  >> /usr/local/etc/php/conf.d/uploads.ini
 
+RUN a2enmod proxy_http proxy_ajp proxy_balancer rewrite headers ssl
+
+RUN mkdir --parents /var/keys/
+RUN mkdir --parents /var/log/apache2/
 
 COPY docker-entrypoint.sh /entrypoint.sh
 COPY apache.conf /etc/apache2/apache2.conf
-# COPY assets/apache-with*.conf /etc/apache2/sites-available/
 
-# there are two updated files
-# because manual installation of egroupware leaves some infos blank
 
 RUN chmod +x /entrypoint.sh
 
 EXPOSE 80 443
 
 ENTRYPOINT ["/entrypoint.sh"]
-CMD [$HOST_NAME]
+CMD [$ACTION, $HOST_NAME]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,19 @@
 set -e
 # made by sneaky of Rothaar Systems (Andre Scholz)
 # V2016-09-03-15-45
-echo "<VirtualHost *:80>" > /etc/apache2/sites-available/000-default.conf
-echo "ServerAdmin webmaster@localhost" >> /etc/apache2/sites-available/000-default.conf
-echo "DocumentRoot /var/www/html" >> /etc/apache2/sites-available/000-default.conf
-echo "</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
-
-if [ -n $HOST_NAME ]; then
+echo "<VirtualHost *:80>" > /etc/apache2/000-default.conf
+echo "ServerAdmin webmaster@localhost" >> /etc/apache2/000-default.conf
+echo "DocumentRoot /var/www/html" >> /etc/apache2/000-default.conf
+echo "</VirtualHost>" >> /etc/apache2/000-default.conf
+echo "Action"
+echo $ACTION
+echo "Host"
+echo $HOST_NAME
+if [ $ACTION  == "installcert" ]; then
+        if [ -z $HOST_NAME ]; then
+                echo "Host is missing"
+                exit 1
+        fi # second parameter is missing
 
         # Apache gets grumpy about PID files pre-existing
         rm -f /var/run/apache2/apache2.pid
@@ -27,12 +34,29 @@ if [ -n $HOST_NAME ]; then
         #--reloadcmd  "service apache2 reload"
         a2ensite apache-with-ssl
         service apache2 stop
+fi # if there should be something (new) registered      
 
-else
-        a2dissite 000-default.conf
-        a2ensite apache-without-ssl 
+if [ $ACTION == "renew" ]; then
+        if [ -z $HOST_NAME ]; then
+                echo "Host is missing" 
+                exit 1
+        fi # second parameter is missing
+        # this is not tested yet!
+        # Apache gets grumpy about PID files pre-existing
+        rm -f /var/run/apache2/apache2.pid
+        service apache2 start
+        mkdir --parents /var/keys/$HOST_NAME
+        cd /root
+        curl https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh >acme.sh
+        chmod +x ./acme.sh
+        acme.sh --renew  -d  $HOST_NAME --force
+        a2dissite 000-default.conf  
+        a2ensite apache-without-ssl    
+        
         a2ensite apache-with-ssl
-fi # if there should be something (new) registered
+        service apache2 stop
+fi # registration should be updated
+
 rm -f /var/run/apache2/apache2.pid
 exec apache2 -DFOREGROUND
 exit 0
